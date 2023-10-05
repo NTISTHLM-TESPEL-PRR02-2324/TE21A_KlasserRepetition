8554aa268ddc6ceffaf96485d81498aa5f8f7182
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2023-10-05 13:42:16 +0200

    Initial commit

---
+++ TE21A_KlasserRepetition/Program.cs
@@ -0,0 +1,20 @@
+﻿Question q1 = new(
+  "5+6", 
+  new List<string>() {"15", "27", "11"},
+  2
+  );
+
+
+// Question q1 = new Question();
+// q1.text = "5+6";
+// q1.answers = new List<string>() { "15", "27", "11" };
+// q1.correct = 2;
+
+// Question q2 = new() {
+//   text = "5+2",
+//   answers = new List<string>() {"8", "6", "9"},
+//   correct = 0
+//  };
+
+
+Console.ReadLine();
